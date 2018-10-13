e37517eb7c84645c067659a1c9b1c3d27e8a82cb
tanjutnao <iamtanjuntao@gmail.com>
2018-10-13 14:50:08 +0800

    更新lambda的值为1,使Hθ能够正常拟合

--- machine-learning-ex2/ex2/ex2_reg.m
+++ machine-learning-ex2/ex2/ex2_reg.m
@@ -108,7 +108,7 @@ pause;
 initial_theta = zeros(size(X, 2), 1);
 
 % Set regularization parameter lambda to 1 (you should vary this)
-lambda = 10000;
+lambda = 1;
 
 % Set Options
 options = optimset('GradObj', 'on', 'MaxIter', 400);
